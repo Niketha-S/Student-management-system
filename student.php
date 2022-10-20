4705d330244877d720867e9dadd492aad496119a
Niketha-S <84368079+Niketha-S@users.noreply.github.com>
2022-10-20 12:58:50 +0530

    Add files via upload

---
+++ student.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/student.css">
+    <style> 
+    body{background-image:url("https://wallpaperaccess.com/full/1567846.jpg");}
+    table {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+th, td {
+  padding: 8px;
+  text-align: left;
+  border-bottom: 1px solid #def;
+}
+
+tr:hover {background-color: #D6EEEE;}
+.button-3 {
+  appearance: none;
+  background-color: #2ea44f;
+  border: 1px solid rgba(27, 31, 35, .15);
+  border-radius: 6px;
+  box-shadow: rgba(27, 31, 35, .1) 0 1px 0;
+  box-sizing: border-box;
+  color: #fff;
+  cursor: pointer;
+  display: inline-block;
+  font-family: -apple-system,system-ui,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji";
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 20px;
+  padding: 6px 16px;
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  vertical-align: middle;
+  white-space: nowrap;
+}
+
+.button-3:focus:not(:focus-visible):not(.focus-visible) {
+  box-shadow: none;
+  outline: none;
+}
+
+.button-3:hover {
+  background-color: #2c974b;
+}
+
+.button-3:focus {
+  box-shadow: rgba(46, 164, 79, .4) 0 0 0 3px;
+  outline: none;
+}
+
+.button-3:disabled {
+  background-color: #94d3a2;
+  border-color: rgba(27, 31, 35, .1);
+  color: rgba(255, 255, 255, .8);
+  cursor: default;
+}
+
+.button-3:active {
+  background-color: #298e46;
+  box-shadow: rgba(20, 70, 32, .2) 0 1px 0 inset;
+}
+body
+{
+    font: italic 1.2em "Fira Sans", serif;
+}
+</style>
+    <title>Result</title>
+</head>
+<body>
+    <?php
+        include("init.php");
+
+        if(!isset($_GET['class']))
+            $class=null;
+        else
+            $class=$_GET['class'];
+        $rn=$_GET['rn'];
+
+        // validation
+        if (empty($class) or empty($rn) or preg_match("/[a-z]/i",$rn)) {
+            if(empty($class))
+                echo '<p class="error">Please select your class</p>';
+            if(empty($rn))
+                echo '<p class="error">Please enter your roll number</p>';
+            if(preg_match("/[a-z]/i",$rn))
+                echo '<p class="error">Please enter valid roll number</p>';
+            exit();
+        }
+
+        $name_sql=mysqli_query($conn,"SELECT `name` FROM `students` WHERE `rno`='$rn' and `class_name`='$class'");
+        while($row = mysqli_fetch_assoc($name_sql))
+        {
+        $name = $row['name'];
+        }
+
+        $result_sql=mysqli_query($conn,"SELECT `p1`, `p2`, `p3`, `p4`, `p5`, `marks`, `percentage` FROM `result` WHERE `rno`='$rn' and `class`='$class'");
+        while($row = mysqli_fetch_assoc($result_sql))
+        {
+            $p1 = $row['p1'];
+            $p2 = $row['p2'];
+            $p3 = $row['p3'];
+            $p4 = $row['p4'];
+            $p5 = $row['p5'];
+            $mark = $row['marks'];
+            $percentage = $row['percentage'];
+        }
+        if(mysqli_num_rows($result_sql)==0){
+            echo "no result";
+            exit();
+        }
+    ?>
+
+    <div class="container">
+        <div class="details">
+            <span>Name:</span> <?php echo $name ?> <br>
+            <span>Class:</span> <?php echo $class; ?> <br>
+            <span>Registration No:</span> <?php echo $rn; ?> <br>
+        </div>
+
+        <div class="main">
+            <table>
+                <tr>
+                    <th>Subjects</th>
+                    <th>Marks</th>
+                </tr>
+                <tr>
+                    <td>Paper 1</td>
+                    <td><?php echo $p1 ?></td>
+                </tr>
+                <tr>
+                    <td>Paper 2</td>
+                    <td><?php echo $p2 ?></td>
+                </tr>
+                <tr>
+                    <td>Paper 3</td>
+                    <td><?php echo $p3 ?></td>
+                </tr>
+                <tr>
+                    <td>Paper 4</td>
+                    <td><?php echo $p4 ?></td>
+                </tr>
+                <tr>
+                    <td>Paper 5</td>
+                    <td><?php echo $p5 ?></td>
+                </tr>
+            </table>
+
+
+
+        <div class="result">
+            <?php echo '<p>Total Marks:&nbsp'.$mark.'</p>';?>
+            <?php echo '<p>Percentage:&nbsp'.$percentage.'%</p>';?>
+        </div>
+    </div>
+    <button class="button-3" onclick="window.print()">Print Result</button>
+
+</body>
+</html>
+
+<!-- HTML !-->
